e266133b7a4067f7a415ca7ba43cb377a428f1e0
Hoang Lam <hoanglam10499@gmail.com>
2021-01-04 02:18:38 +0700

    update 0.0.3

--- android/src/main/java/com/reactlibrary/BrightnessPackage.java
+++ android/src/main/java/com/reactlibrary/BrightnessPackage.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -14,6 +15,12 @@ public class BrightnessPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new BrightnessModule(reactContext));
     }
+
+    // @Override
+    // public List<Class<? extends JavaScriptModule>> createJSModules() {
+    //     return null;
+    // }
+
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
